b7fc1659d0b59c62c61cf3edf63aa2880175d24f
nendix <nendix12@gmail.com>
2024-09-18 15:00:33 +0200

    added stow

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,33 +1,4 @@
--- config reload
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "R", function()
-	hs.reload()
-end)
 hs.alert.show("Config loaded")
-
--- show a clock
-hs.loadSpoon("AClock")
-spoon.AClock:init()
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "C", function()
-	spoon.AClock:toggleShow()
-end)
-
--- caffeine extension
--- function setCaffeineDisplay(state)
--- 	if state then
--- 		caffeine:setTitle("AWAKE")
--- 	else
--- 		caffeine:setTitle("SLEEPY")
--- 	end
--- end
---
--- function caffeineClicked()
--- 	setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
--- end
---
--- if caffeine then
--- 	caffeine:setClickCallback(caffeineClicked)
--- 	setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
--- end
 
 keyUpDown = function(modifiers, key)
 	hs.eventtap.keyStroke(modifiers, key, 0)
