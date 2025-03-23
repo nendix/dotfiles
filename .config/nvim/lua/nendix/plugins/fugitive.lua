3274377cb519cc9baa17d10a246656a58eddbdbc
nendix <nendix12@gmail.com>
2025-03-23 13:22:26 +0100

    fix

--- .config/nvim/lua/nendix/plugins/fugitive.lua
+++ .config/nvim/lua/nendix/plugins/fugitive.lua
@@ -7,7 +7,7 @@ return {
 		vim.keymap.set("n", "<leader>gaa", "<CMD>Git add --all<CR>", { desc = "Git add all" })
 		vim.keymap.set("n", "<leader>gaf", "<CMD>Gwrite<CR>", { desc = "Git add file" })
 		vim.keymap.set("n", "<leader>gc", "<CMD>Git commit<CR>", { desc = "Git commit" })
-		vim.keymap.set("n", "<leader>gC", ':Git commit -m "', { desc = "Git commit" })
+		vim.keymap.set("n", "<leader>gC", ":Git commit -m", { desc = "Git commit" })
 		vim.keymap.set("n", "<leader>gp", "<CMD>Git push<CR>", { desc = "Git push" })
 
 		vim.keymap.set("n", "<leader>gr", "<CMD>Git reset<CR>", { desc = "Git reset" })
