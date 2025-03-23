22ad740010197ee9f50a85ea3dddea72fd675338
nendix <nendix12@gmail.com>
2025-03-23 21:08:28 +0100

    fixed trouble plugin

--- .config/nvim/lua/nendix/plugins/trouble.lua
+++ .config/nvim/lua/nendix/plugins/trouble.lua
@@ -1,19 +1,28 @@
 return {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
-	opts = {
-		focus = true,
-	},
-	cmd = "Trouble",
-	keys = {
-		{ "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open trouble workspace diagnostics" },
-		{
-			"<leader>xd",
-			"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
-			desc = "Open trouble document diagnostics",
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+		event = "VeryLazy",
+		opts = {
+			focus = true,
 		},
-		{ "<leader>xq", "<cmd>Trouble quickfix toggle<CR>", desc = "Open trouble quickfix list" },
-		{ "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
-		{ "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
+		cmd = "Trouble",
+		keys = {
+			{ "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open trouble workspace diagnostics" },
+			{
+				"<leader>xd",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
+				desc = "Open trouble document diagnostics",
+			},
+			{ "<leader>xq", "<cmd>Trouble quickfix toggle<CR>", desc = "Open trouble quickfix list" },
+			{ "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
+			{ "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
+		},
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		event = "VeryLazy",
+		opts = {},
 	},
 }
