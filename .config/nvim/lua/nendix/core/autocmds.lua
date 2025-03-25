02e94eb67c3adbc8ba88e3d67c793f33dc2112b7
nendix <nendix12@gmail.com>
2025-03-25 13:13:32 +0100

    removed all autocmsd

--- .config/nvim/lua/nendix/core/autocmds.lua
+++ .config/nvim/lua/nendix/core/autocmds.lua
@@ -1,12 +1,1 @@
--- trigger treesitter for go files
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "go",
-	callback = function()
-		vim.treesitter.start()
-	end,
-})
-
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = "*.templ",
-	command = "set filetype=go",
-})
+-- add autocmds here
