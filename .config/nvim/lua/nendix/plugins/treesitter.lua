1fbf793016b3e7f208b97e923e19643c81b158c5
nendix <nendix12@gmail.com>
2025-03-25 12:43:14 +0100

    fix: treesitter

--- .config/nvim/lua/nendix/plugins/treesitter.lua
+++ .config/nvim/lua/nendix/plugins/treesitter.lua
@@ -1,40 +1,52 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPre", "BufNewFile" },
-		build = ":TSUpdate",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-			"windwp/nvim-ts-autotag",
-		},
-		config = function() -- configure treesitter
-			require("nvim-treesitter").setup({
-				-- enable syntax highlighting
-				highlight = { enable = true },
-				-- enable indentation
-				indent = { enable = true },
-				-- enable autotagging (w/ nvim-ts-autotag plugin)
-				autotag = { enable = true },
-				-- ensure these language parsers are installed
-				ensure_installed = {
-					"json",
-					"javascript",
-					"tsx",
-					"yaml",
-					"html",
-					"css",
-					"c",
-					"markdown",
-					"markdown_inline",
-					"go",
-					"bash",
-					"lua",
-					"vim",
-					"gitignore",
-				},
-				-- auto install above language parsers
-				auto_install = true,
-			})
-		end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		"windwp/nvim-ts-autotag",
 	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			-- enable syntax highlighting
+			highlight = { enable = true },
+			-- enable indentation
+			indent = { enable = true },
+			-- enable autotagging (w/ nvim-ts-autotag plugin)
+			autotag = { enable = true },
+			-- ensure these language parsers are installed
+
+			-- A list of parser names, or "all"
+			ensure_installed = {
+				"json",
+				"javascript",
+				"html",
+				"css",
+				"c",
+				"markdown",
+				"go",
+				"java",
+				"bash",
+				"lua",
+				"vim",
+				"gitignore",
+				"python",
+			},
+
+			auto_install = true,
+
+			-- Install parsers synchronously (only applied to `ensure_installed`)
+			sync_install = false,
+		})
+
+		local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+		treesitter_parser_config.templ = {
+			install_info = {
+				url = "https://github.com/vrischmann/tree-sitter-templ.git",
+				files = { "src/parser.c", "src/scanner.c" },
+				branch = "master",
+			},
+		}
+
+		vim.treesitter.language.register("templ", "templ")
+	end,
 }
