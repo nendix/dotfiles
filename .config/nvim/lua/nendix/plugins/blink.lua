b57a87ad8cd01303dca0ae59585fc4f3b86bb241
nendix <nendix12@gmail.com>
2025-04-14 01:50:18 +0200

    removed useless comments

--- .config/nvim/lua/nendix/plugins/blink.lua
+++ .config/nvim/lua/nendix/plugins/blink.lua
@@ -7,13 +7,6 @@ return {
 	version = "1.*",
 
 	opts = {
-		-- All presets have the following mappings:
-		-- C-space: Open menu or open docs if already open
-		-- C-n/C-p or Up/Down: Select next/previous item
-		-- C-e: Hide menu
-		-- C-k: Toggle signature help (if signature.enabled = true)
-		--
-		-- See :h blink-cmp-config-keymap for defining your own keymap
 		keymap = {
 			preset = "none",
 
