e23706769e352b39dec14bffe8f1eede2a04eebb
nendix <nendix12@gmail.com>
2025-04-15 20:49:23 +0200

    minor changes to colorscheme

--- .config/nvim/lua/nendix/plugins/colorscheme.lua
+++ .config/nvim/lua/nendix/plugins/colorscheme.lua
@@ -63,18 +63,6 @@ return {
 		end,
 	},
 	{
-		"zenbones-theme/zenbones.nvim",
-		dependencies = "rktjmp/lush.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.g.zenbones = {
-				transparent_background = true,
-			}
-			-- vim.cmd.colorscheme("zenbones")
-		end,
-	},
-	{
 		"gmr458/cold.nvim",
 		lazy = false,
 		priority = 1000,
