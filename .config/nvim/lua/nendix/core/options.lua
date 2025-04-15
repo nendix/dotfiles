ae5422c00a5a8b558d1f404893d1aaa40f9f2e51
nendix <nendix12@gmail.com>
2025-04-15 20:50:26 +0200

    removed color column

--- .config/nvim/lua/nendix/core/options.lua
+++ .config/nvim/lua/nendix/core/options.lua
@@ -29,7 +29,7 @@ opt.cursorline = true -- highlight the current cursor line
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
-opt.colorcolumn = "80"
+-- opt.colorcolumn = "80"
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
