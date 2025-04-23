b7eb9b797d7f3489a90c7714c9f27f40de81258a
nendix <nendix12@gmail.com>
2025-04-23 12:32:08 +0200

    tmuxc colorsssss

--- .config/nvim/lua/nendix/plugins/colorscheme.lua
+++ .config/nvim/lua/nendix/plugins/colorscheme.lua
@@ -2,42 +2,17 @@ return {
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
+		lazy = false,
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
 			require("catppuccin").setup({
 				flavour = "mocha", -- latte, frappe, macchiato, mocha
-				background = { -- :h background
-					light = "latte",
-					dark = "mocha",
-				},
 				transparent_background = true,
-				show_end_of_buffer = false, -- show the '~' characters after the end of buffers
 				term_colors = true,
-				dim_inactive = {
-					enabled = false,
-					shade = "dark",
-					percentage = 0.15,
-				},
-				no_italic = false, -- Force no italic
-				no_bold = false, -- Force no bold
-				no_underline = false, -- Force no underline
 				styles = {
 					comments = { "italic" },
-					conditionals = {},
-					loops = {},
-					functions = {},
-					keywords = {},
-					strings = {},
-					variables = {},
-					numbers = {},
-					booleans = {},
-					properties = {},
-					types = {},
-					operators = {},
 				},
-				color_overrides = {},
-				custom_highlights = {},
 				integrations = {
 					alpha = true,
 					blink_cmp = true,
@@ -73,16 +48,6 @@ return {
 				italic_keyword = true,
 			})
 			-- vim.cmd.colorscheme("cold")
-		end,
-	},
-	{
-		"mellow-theme/mellow.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.g.mellow_transparent = true
-			vim.g.mellow_italic_functions = true
-			-- vim.cmd.colorscheme("mellow")
 		end,
 	},
 }
