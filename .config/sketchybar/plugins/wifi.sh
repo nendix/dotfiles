24b77fe30f056fa31a9139dc293844f46c31ea02
nendix <nendix12@gmail.com>
2025-05-12 23:09:39 +0200

    front_app change and wifi fix in sketchybar

--- .config/sketchybar/plugins/wifi.sh
+++ .config/sketchybar/plugins/wifi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 WIFI_STATUS=$(networksetup -getairportpower en0 | awk '{print $4}')
-WIFI_SSID=$(networksetup -getairportnetwork en0 | awk '{print $4}')
+WIFI_SSID=$(networksetup -getairportnetwork en0)
 
-if [[ "$WIFI_STATUS" == "On" && -n "$WIFI_SSID" ]]; then
+if [[ "$WIFI_STATUS" == "On" ]]; then
   ICON="󰖩"  # Connected
 else
   ICON="󰖪"  # Disconnected
