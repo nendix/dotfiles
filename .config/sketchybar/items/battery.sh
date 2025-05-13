fe0ec01bb974f485c8aa9484f8d125a111f3e225
nendix <nendix12@gmail.com>
2025-05-13 14:40:24 +0200

    new battery module sketchybar

--- .config/sketchybar/items/battery.sh
+++ .config/sketchybar/items/battery.sh
@@ -2,5 +2,5 @@
 
 sketchybar --add item battery right \
            --set battery icon.padding_right=3 label.padding_left=3 \
-           update_freq=300 script="$PLUGIN_DIR/battery.sh" \
+           update_freq=120 script="$PLUGIN_DIR/battery.sh" \
            --subscribe battery system_woke power_source_change
