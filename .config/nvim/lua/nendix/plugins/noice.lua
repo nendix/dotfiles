6c9b8ec8ddd231f79f8ae509a54088fa71985257
nendix <nendix12@gmail.com>
2025-05-13 12:32:37 +0200

    noice default search in bottom bar

--- .config/nvim/lua/nendix/plugins/noice.lua
+++ .config/nvim/lua/nendix/plugins/noice.lua
@@ -19,12 +19,12 @@ return {
 				override = {
 					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true,
+					["blink.entry.get_documentation"] = true,
 				},
 			},
 			-- you can enable a preset for easier configuration
 			presets = {
-				bottom_search = false, -- use a classic bottom cmdline for search
+				bottom_search = true, -- use a classic bottom cmdline for search
 				command_palette = true, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
