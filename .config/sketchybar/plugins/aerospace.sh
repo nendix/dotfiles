f3a08852557de9c452790657b6d0ee1d2f907d5d
nendix <nendix12@gmail.com>
2025-05-14 23:01:18 +0200

    new icon map fn SB

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# make sure it's executable with:
-# chmod +x ~/.config/sketchybar/plugins/aerospace.sh
-
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
     sketchybar --set $NAME background.drawing=on
 else
