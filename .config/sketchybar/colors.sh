2306c601f43d9a956238c3356b56a3d93e671f5f
nendix <nendix12@gmail.com>
2025-05-16 12:56:56 +0200

    added colors in SB

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -4,21 +4,11 @@ FOREGROUND=0xffadacac
 BORDER=0xff333333
 
 # normal
-palette=0=#111111
-palette=1=#9c797d
-palette=2=#8a968f
-palette=3=#b5ae84
-palette=4=#808796
-palette=5=#8a879c
-palette=6=#b0b58a
-palette=7=#adacac
-
-# bright
-palette=8=#696969
-palette=9=#9c797d
-palette=10=#8a968f
-palette=11=#b5ae84
-palette=12=#808796
-palette=13=#a7ab93
-palette=14=#b0b58a
-palette=15=#adacac
+BLACK=0xff111111
+RED=0xff9c797d
+GREEN=0xff8a968f
+YELLOW=0xff#b5ae84
+BLUE=0xff808796
+MAGENTA=0xff8a879c
+CYAN=0xffb0b58a
+WHITE=0xffadacac
