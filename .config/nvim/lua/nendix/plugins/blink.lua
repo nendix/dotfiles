87034d3c11593484239bce1fc3fa46c4f57aa4e6
nendix <nendix12@gmail.com>
2025-05-20 11:44:59 +0200

    blink remapping

--- .config/nvim/lua/nendix/plugins/blink.lua
+++ .config/nvim/lua/nendix/plugins/blink.lua
@@ -1,17 +1,17 @@
 return {
 	"saghen/blink.cmp",
-	-- optional: provides snippets for the snippet source
 	dependencies = { "rafamadriz/friendly-snippets" },
-
 	-- use a release tag to download pre-built binaries
 	version = "1.*",
-
 	opts = {
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
 		keymap = {
 			preset = "none",
 
 			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-			["<C-e>"] = { "hide", "fallback" },
+			["<C-c>"] = { "hide", "fallback" },
 
 			["<Tab>"] = {
 				function(cmp)
@@ -26,25 +26,21 @@ return {
 			},
 			["<S-Tab>"] = { "snippet_backward", "fallback" },
 
-			["<Up>"] = { "select_prev", "fallback" },
-			["<Down>"] = { "select_next", "fallback" },
 			["<C-k>"] = { "select_prev", "fallback_to_mappings" },
 			["<C-j>"] = { "select_next", "fallback_to_mappings" },
 
 			["<C-p>"] = { "scroll_documentation_up", "fallback" },
 			["<C-n>"] = { "scroll_documentation_down", "fallback" },
 
-			["<C-b>"] = { "show_signature", "hide_signature", "fallback_to_mappings" },
+			["<C-v>"] = { "show_signature", "hide_signature", "fallback_to_mappings" },
 		},
 		cmdline = {
 			keymap = { preset = "inherit" },
 			completion = { menu = { auto_show = true }, ghost_text = { enabled = true } },
 		},
-
 		appearance = {
 			nerd_font_variant = "mono",
 		},
-
 		completion = {
 			menu = { border = "rounded" },
 			documentation = { auto_show = false, window = { border = "rounded" } },
