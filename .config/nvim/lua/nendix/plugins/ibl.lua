5161a970e3582a72185f7017536b570bfa33d19c
nendix <nendix12@gmail.com>
2026-04-01 17:09:31 +0200

    ibl scope

--- .config/nvim/lua/nendix/plugins/ibl.lua
+++ .config/nvim/lua/nendix/plugins/ibl.lua
@@ -6,7 +6,7 @@ return {
 	config = function()
 		require("ibl").setup({
 			indent = { char = "│" },
-			scope = { enabled = false },
+			scope = { enabled = true, show_start = false, show_end = false },
 		})
 	end,
 }
